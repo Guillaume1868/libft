d17e088110e41cf9ef8d07d61ad285c6573594f1
gaubert <guillaume@aubert.ws>
2021-10-12 09:56:05 +0200

    protect from null params ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gaubert <gaubert@student.42lausann>        +#+  +:+       +#+        */
+/*   By: gaubert <gaubert@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 11:21:46 by gaubert           #+#    #+#             */
-/*   Updated: 2021/10/11 12:24:42 by gaubert          ###   ########.fr       */
+/*   Updated: 2021/10/12 09:55:35 by gaubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return;
 	while (*s != '\0')
 	{
 		write(fd, s, 1);
