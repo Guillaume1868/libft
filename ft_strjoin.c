c6e5cf87dc25070fabda50a2d819a3463946295f
Guillaume <guillaume@aubert.ws>
2021-10-18 09:14:11 +0200

    Added a sizeof in strjoin's malloc

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	res = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	res = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
 	ft_memcpy(res, s1, ft_strlen(s1));
